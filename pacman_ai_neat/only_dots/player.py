18de50cfe71923fcbe37149d2fd6c858199de11c
RJW20 <robertjwatts84@outlook.com>
2024-05-10 12:31:15 +0100

    Implement looking for pacdots

--- pacman_ai_neat/only_dots/player.py
+++ pacman_ai_neat/only_dots/player.py
@@ -3,6 +3,18 @@ from pacman_app.map import Direction
 from neat import BasePlayer
 
 
+def in_bounds(tile: tuple[int,int]) -> bool:
+    """Return True if the given tile is in the PacMaze."""
+
+    if tile[0] < 2 or tile[0] > 27:
+        return False
+    
+    if tile[1] < 4 or tile[1] > 32:
+        return False
+    
+    return True
+
+
 class Player(PacMan, BasePlayer):
 
     def __init__(self, *player_args: dict) -> None:
@@ -16,19 +28,26 @@ class Player(PacMan, BasePlayer):
         and whether there is food in any of the cardinal directions (4 one-hot values).
         """
         self.vision = []
-        
-        # Own position
-        self.vision.extend(position_to_float(self.position))
 
-        # 4 cardinal directions
+        # Move in 4 cardinal directions
         directions = [Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT]
         for direction in directions:
             self.vision.append(int(self.can_move_in_direction(direction)))
 
-        # Ghosts' positions
-        for ghost in ghosts:
-            self.vision.extend(position_to_float(ghost.position))
+        # Pacdot in 4 directions
+        for direction in directions:
+            next_tile = self.position.tile_x + direction.value.d_x, self.position.tile_y + direction.value.d_y
+            found = False
+            while in_bounds(next_tile):
+                if next_tile in pacdots.dots:
+                    self.vision.append(1)
+                    found = True
+                    break
+                else:
+                    next_tile = next_tile[0] + direction.value.d_x, next_tile[1] + direction.value.d_y
 
+            if not found:
+                self.vision.append(0)
 
     def think(self) -> Direction:
         """Feed the input into the Genome and return the output as a valid move."""
