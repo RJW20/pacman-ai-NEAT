dcb5a152f40d659b2b4c173b7cf33a196d57a09b
RJW20 <robertjwatts84@outlook.com>
2024-05-15 11:27:43 +0100

    Bind simulator functions to phases

--- pacman_ai_neat/phase.py
+++ pacman_ai_neat/phase.py
@@ -1,4 +1,8 @@
 from enum import Enum
+from typing import Callable
+
+from pacman_ai_neat.simulator import only_dots
+from pacman_ai_neat.player import Player
 
 
 class Phase(Enum):
@@ -8,5 +12,17 @@ class Phase(Enum):
     PowerDots respectively.
     """
 
-    ONLY_DOTS = [True, False, False, False]
+    ONLY_DOTS = [True, False, False, False], only_dots
+
+    def __new__(cls, *args, **kwargs) -> object:
+        obj = object.__new__(cls)
+        obj._value_ = args[0]
+        return obj
+    
+    def __init__(self, inclusions: list[bool], simulator_function: Callable[[Player], Player]) -> None:
+        self._simulator_function_ = simulator_function
+
+    @property
+    def simulator_function(self) -> Callable[[Player], Player]:
+        return self._simulator_function_
     
